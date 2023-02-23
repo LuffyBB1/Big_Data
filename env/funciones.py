1693c1f7f14f9ebe2ad25700c70f6b27cf43230f
Brayan Esteven Bernal Martínez <100706145+LuffyBB1@users.noreply.github.com>
2023-02-23 15:39:26 -0500

    Delete env directory

--- env/funciones.py
+++
@@ -1,9 +0,0 @@
-"""
-Functions Module
-"""
-def sumar(x, y):
-    return x + y
-    
-
-def es_primo(x):
-    return False
--- env/test_funciones.py
+++
@@ -1,5 +0,0 @@
-from funciones import sumar, es_primo
-def test_sumar():
-    assert sumar(2,3)==5
-    assert sumar(2,2)==4
-    assert sumar(1,2)==3
